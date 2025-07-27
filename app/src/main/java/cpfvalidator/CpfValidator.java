f26e06ed62a3c50024d041a07eaf7d1231cd9581
Felipe Salome <felipesalome_ti@outlook.com>
2025-07-27 12:30:46 -0300

    [feat] Refatorado CpfValidator

--- app/src/main/java/cpfvalidator/CpfValidator.java
+++ app/src/main/java/cpfvalidator/CpfValidator.java
@@ -5,34 +5,37 @@ public class CpfValidator {
     private static final int CPF_FIRST_DIGIT_VERIFIER = 9;
     private static final int CPF_SECOND_DIGIT_VERIFIER = 10;
 
+    private final CpfPreprocessor preprocessor;
+    private final CpfRepeatedDigitsValidator repeatedDigitsValidator;
+    private final CpfCheckDigitCalculator checkDigitCalculator;
+
+    public CpfValidator() {
+        preprocessor = new CpfPreprocessor();
+        repeatedDigitsValidator = new CpfRepeatedDigitsValidator();
+        checkDigitCalculator = new CpfCheckDigitCalculator();
+    }
+
     public boolean isValid(String pCpf) {
         if (pCpf == null || pCpf.isEmpty()) {
             return false;
         }
 
-        CpfPreprocessor preprocessor = new CpfPreprocessor();
         String cpfSanitized = preprocessor.clean(pCpf);
 
         if (!preprocessor.isValidLength(cpfSanitized)) {
             return false;
         }
 
-        CpfRepeatedDigitsValidator repeatedDigitsValidator = new CpfRepeatedDigitsValidator();
         if (repeatedDigitsValidator.hasRepeatedDigits(cpfSanitized)) {
             return false;
         }
 
-        CpfCheckDigitCalculator checkDigitCalculator = new CpfCheckDigitCalculator();
         String firstCheckDigit = String.valueOf(checkDigitCalculator.calculateFirstDigit(cpfSanitized));
         if (!firstCheckDigit.equals(cpfSanitized.substring(CPF_FIRST_DIGIT_VERIFIER, CPF_SECOND_DIGIT_VERIFIER))) {
             return false;
         }
 
         String secondCheckDigit = String.valueOf(checkDigitCalculator.calculateSecondDigit(cpfSanitized));
-        if (!secondCheckDigit.equals(cpfSanitized.substring(CPF_SECOND_DIGIT_VERIFIER))) {
-            return false;
-        }
-
-        return true;
+        return secondCheckDigit.equals(cpfSanitized.substring(CPF_SECOND_DIGIT_VERIFIER));
     }
 }
