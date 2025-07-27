e1344628c2eb77da07832d4255b4600d5f173bfa
Felipe Salome <felipesalome_ti@outlook.com>
2025-07-27 01:03:04 -0300

    [fix] Ajuste no coverage em CpfCheckDigitCalculator

--- app/src/test/java/cpfvalidator/CpfCheckDigitCalculatorTest.java
+++ app/src/test/java/cpfvalidator/CpfCheckDigitCalculatorTest.java
@@ -3,15 +3,21 @@ package cpfvalidator;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class CpfCheckDigitCalculatorTest {
+    private static CpfCheckDigitCalculator instance;
+
+    @BeforeAll static void begin() {
+        instance = new CpfCheckDigitCalculator();
+    }
+
     @Test void shouldReturnFirstCheckDigit() {
         int expected = 3;
 
-        CpfCheckDigitCalculator instance = new CpfCheckDigitCalculator();
         int actual = instance.calculateFirstDigit("13776789034");
 
         assertEquals(expected, actual);
@@ -20,7 +26,6 @@ class CpfCheckDigitCalculatorTest {
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 4, 5, 6, 7, 8, 9, 0})
     void shouldReturnFirstCheckDigitInvalid(int pUnexpected) {
-        CpfCheckDigitCalculator instance = new CpfCheckDigitCalculator();
         int actual = instance.calculateFirstDigit("13776789034");
 
         assertNotEquals(pUnexpected, actual);
@@ -29,7 +34,6 @@ class CpfCheckDigitCalculatorTest {
     @Test void shouldReturnSecondCheckDigit() {
         int expected = 4;
 
-        CpfCheckDigitCalculator instance = new CpfCheckDigitCalculator();
         int actual = instance.calculateSecondDigit("13776789034");
 
         assertEquals(expected, actual);
@@ -38,9 +42,24 @@ class CpfCheckDigitCalculatorTest {
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3, 5, 6, 7, 8, 9, 0})
     void shouldReturnSecondCheckDigitInvalid(int pUnexpected) {
-        CpfCheckDigitCalculator instance = new CpfCheckDigitCalculator();
         int actual = instance.calculateSecondDigit("13776789034");
 
         assertNotEquals(pUnexpected, actual);
+    }
+
+    @Test void shouldReturnZeroForFirstDigit() {
+        int expected = 0;
+
+        int actual = instance.calculateFirstDigit("04978388007");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test void shouldReturnZeroForSecondDigit() {
+        int expected = 0;
+
+        int actual = instance.calculateSecondDigit("75484920060");
+
+        assertEquals(expected, actual);
     }
 }
